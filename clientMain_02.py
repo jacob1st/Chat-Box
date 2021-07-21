bc2337149e2095fbc51402b07bf71ff42272d61d
jacob1st <87782445+jacob1st@users.noreply.github.com>
2021-07-21 15:20:59 -0500

    Update clientMain_02.py

--- clientMain_02.py
+++ clientMain_02.py
@@ -4,13 +4,10 @@
 import socket
 import threading
 import tkinter as tk
-# from tkinter.constants import BOTTOM, COMMAND, LEFT
-# from typing import Collection, Text, final
-
-# Server connection information
 
 PORT = 6666
-ADDR = "192.168.1.121"
+# Change addr to the adress that the server is being hosted on (e.x. ADDR = "192.168.1.2")
+ADDR = ""
 FORMAT = 'utf-8'
 HEADER = 64
 
@@ -100,101 +97,3 @@ start()
 
 root.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ###IDEAS
-# # scrollwheel, disconnect needds to close, admin perms, customization
-# # wrap length for label
-
-# import socket
-# import threading
-# import tkinter as tk
-# from tkinter.constants import BOTTOM, COMMAND, LEFT
-# from typing import Collection, Text, final
-
-# PORT = 6666
-# ADDR = "192.168.1.121"
-# FORMAT = 'utf-8'
-# HEADER = 64
-
-# HEIGHT = 700
-# WIDTH = 800
-
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client.connect((ADDR, PORT))
-
-
-# def listen_for_msg():
-#     while root.state() == 'normal':
-#         final_message = client.recv(2048).decode(FORMAT) + "\n"
-#         if final_message.startswith("!"):
-#             break
-#         texts_label['text'] += final_message
-#         if len(texts_label['text']) > 400:
-#             texts_label['text'] = final_message
-
-
-# def send(msg):
-#     message = msg.encode(FORMAT)
-#     msg_length = len(message)
-#     send_length = str(msg_length).encode(FORMAT)
-#     send_length += b' ' * (HEADER - len(send_length))
-#     client.send(send_length)
-#     client.send(message)
-
-# def sending_messages(entry_message):
-#     send(entry_message)
-#     entry.delete(0, 'end')
-#     if entry_message == "!DISCONNECT":
-#         root.destroy()
-
-# def start():
-#     recv_thread = threading.Thread(target=listen_for_msg)
-#     recv_thread.start()
-
-
-# root = tk.Tk()
-
-# canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
-# canvas.pack()
-
-# # Black frame (for entry)
-# full_frame = tk.Frame(root, bg='#506D05')
-# full_frame.place(relx=0, rely=0, relwidth=1, relheight=1)
-
-# # Blue frame
-# frame = tk.Frame(root, bg='#1F208D')
-# frame.place(relx=0, rely=0, relwidth=1, relheight=0.9)
-
-# #label for messages in Blue Frame
-# texts_label = tk.Label(frame, bg="#1F208D", fg="white", wraplength=600, relief="ridge", font="Calibri 15", anchor="nw", justify="left", border=4)
-# texts_label.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.8)
-
-# # Send button
-# button = tk.Button(full_frame, text="send", font="Arial 12", bg="#B673E8", command=lambda: sending_messages(entry.get()))
-# button.place(relx=.9, rely=0.9, relwidth=.1, relheight=.1)
-
-# # Message label
-# label = tk.Label(full_frame, text="Message: ", relief="raised", bg="#B673E8", font="Arial 12")
-# label.place(relx=0, rely=0.9, relwidth=.1, relheight=.1)
-
-# # To send message
-# entry = tk.Entry(full_frame, bg='black', fg='white', relief="ridge", font="Calibri 13", insertbackground='white')
-# entry.place(relx=0.1, rely=.9, relwidth=.8, relheight=.1) 
-
-# start()
-
-# root.mainloop()
